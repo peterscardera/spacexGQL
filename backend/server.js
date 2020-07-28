7aab83d7759cef2f3a6dfc9c2f7e8cc5bcd575d7
PeterScardera <peterscardera@gmail.com>
2020-07-28 09:44:02 -0400

    added schema

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,23 @@
-const { ApolloServer } = require("apollo-server")
+const { ApolloServer } = require("apollo-server-express");
 const express = require("express");
 const expressPlayground = require("graphql-playground-middleware-express")
-  .default;
-const { readFileSync } = require("fs");
+    .default;
 
-const resolvers = require("./resolvers");
+const typeDefs = require("./schema");
+
+const app = express();
+
+const server = new ApolloServer({
+    typeDefs,
+});
+
+server.applyMiddleware({ app });
+
+app.get("/playground", expressPlayground({ endpoint: "/graphql" }));
+app.get("/", (req, res) => res.end("Welcome to the PhotoShare API"));
+
+app.listen({ port: 4000 }, () =>
+    console.log(
+        `GraphQL Server running @ http://localhost:4000${server.graphqlPath}`
+    )
+);
